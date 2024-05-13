2b589ee040779e8a854a3776944f0fe24a9dba41
khalilBahi <7753@holbertonstudents.com>
2024-05-13 17:08:28 +0200

    Inheritance

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -5,6 +5,9 @@ export default class SkyHighBuilding extends Building {
     super(sqft);
     this._floors = floors;
   }
+  set floors(floors) {
+    this._floors = floors;
+  }
 
   get floors() {
     return this._floors;
