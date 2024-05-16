11cf1f94c0addc63219ba6981877b452066830b1
khalilBahi <7753@holbertonstudents.com>
2024-05-16 16:18:57 +0200

    script that prints the title of a Star Wars movie where the episode number matches a given integer

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
-export default function handleProfileSignup () {
+
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch(() => {
-      return (new Error('Signup system offline'));
-    });
+    .catch(() => (new Error('Signup system offline')));
 }
